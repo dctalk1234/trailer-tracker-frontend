f0ec3f7e1df81d24f640948ed5647f88f5f58c68
James Opara <jamesopara@Jamess-MacBook-Pro.local>
2019-10-21 15:10:54 -0500

    committing so i can pull

--- trailertracker/src/App.js
+++ trailertracker/src/App.js
@@ -1,51 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Route, Link, Switch } from 'react-router-dom';
-import About from './About';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Link, Switch } from "react-router-dom";
+import About from "./About";
 import Movie from "./Movie";
 import CategoryList from "./CategoryList";
 import OneCategory from "./OneCategory";
 import catList from "./categoryData.json";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
     this.state = {
-        cats: catList
+      cats: catList
     };
-    }
-    showCategory(film) {
-        this.setState({
-            title: film,
-            trailer: film
-        })
-    }
+  }
+  showCategory(film) {
+    this.setState({
+      title: film,
+      trailer: film
+    });
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <header>
-                    <nav>
-                        <Link to="/">
-                            Home
-                        </Link>
-                        <Link to="/about">
-                            About
-                        </Link>
-
-                    </nav>
-                </header>
-                <main>
-                    <Switch>
-                        <Route path="/" exact={true} component={CategoryList}/>
-                        <Route path="/about" exact={true} component={About}/>
-                        <Route path="/movie/:title" exact={true} render={routerProps => <Movie film={this.showCategory} match={routerProps.match}/>}/>
-                        <Route path="/category" exact={true} component={OneCategory}/>
-                    </Switch>
-                </main>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+          </nav>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/" exact={true} component={CategoryList} />
+            <Route path="/about" exact={true} component={About} />
+            <Route
+              path="/movie/:title"
+              exact={true}
+              render={routerProps => (
+                <Movie film={this.showCategory} match={routerProps.match} />
+              )}
+            />
+            <Route path="/category" exact={true} component={OneCategory} />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
 }
 export default App;
