e2094d8028896708e76dffc15f42c46f89162411
James Opara <jamesopara@Jamess-MacBook-Pro.local>
2019-10-21 09:59:16 -0500

    initial category list and single category page

---
+++ trailertracker/src/OneCategory.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Link from "react-router-dom";
+
+
+class OneCategory extends Component {
+    render() {
+        return(
+            <div>
+                <h3>My Category!</h3>
+            </div>
+        )
+    }
+}
+
+
+export default OneCategory;
