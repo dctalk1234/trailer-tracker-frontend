5fedb235e2b94adabd3d9db27c1de5798ad73eef
spiano <lafleurpiano@gmail.com>
2019-10-21 11:54:17 -0500

    updated movieData to reflect working youtube trailers

--- trailertracker/src/Movie.js
+++ trailertracker/src/Movie.js
@@ -7,9 +7,20 @@ export default class Movie extends Component {
             return (
                 <div >
                     <h3>{movie.title}</h3>
+                    <iframe
+                        width="627"
+                        height="470"
+                        src={movie.trailer}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen>
+
+                    </iframe>
+
+
                 </div>
             );
-        
+
         return <div>
             <h2>Checkout these movies!</h2>
 
