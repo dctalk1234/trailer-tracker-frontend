938983ce1c31882950e5c478dd2f00772a3ff5af
Nathaniel Stack <nathanielstack@Nathaniels-MacBook-Pro.local>
2019-10-22 07:34:02 -0700

    added link to backend for posting a new category

--- trailertracker/src/NewCategory.js
+++ trailertracker/src/NewCategory.js
@@ -1,47 +1,56 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 import catList from "./categoryData.json";
-import movieList from './movieData';
+import movieList from "./movieData";
 
 export default class NewCategory extends Component {
-    constructor() {
-        super();
-        this.state = {
-            category: ''
-        };
-    }
-    handleChange(evt) {
-        axios.post(
-            'https://localhost:8080/category',
-            { title: 'HAHAHA' },
-        { headers: { 'Content-Type': 'application/json'} }
-        )
-            .then(res => {
-                console.log(res);
-            })
-    }
-    render() {
-        // let filteredMovie = catList[0].movies.filter(
-        //     (movieTitle) => {
-        //         return movieTitle.title.indexOf(this.state.search) !== -1;
-        //     }
-        // );
-        return (
-            <div>
-                <form>
-            <label>Category <input type="text" placeholder="type category name" /> </label>
-                    <button onSubmit={this.handleChange} type="submit">Submit</button>
-                </form>
-                <label>Movies <input type="text" placeholder="type movie title" value={this.state.search}/> </label>
+	constructor() {
+		super();
+		this.state = {
+			category: ""
+		};
+	}
+	handleChange(evt) {
+        console.log(evt);
+        evt.preventDefault();
+        
+        axios.post("http://localhost:8080/Category/", 
+        { title: "HAHAHA" }, 
+        { headers: { "Content-Type": "application/json" } })
+        .then(res => {
+			console.log(res);
+		}).catch(err => {
+            console.error(err);
+        });
+	}
+	render() {
+		// let filteredMovie = catList[0].movies.filter(
+		//     (movieTitle) => {
+		//         return movieTitle.title.indexOf(this.state.search) !== -1;
+		//     }
+		// );
+		return (
+			<div>
+				<form>
+					<label>
+						Category <input type="text" placeholder="type category name" />{" "}
+					</label>
+					<button onClick={this.handleChange} type="submit">
+						Submit
+					</button>
+				</form>
+				<label>
+					Movies <input type="text" placeholder="type movie title" value={this.state.search} />{" "}
+				</label>
 
-                <div>
-                    {/*<ul >*/}
-                    {/*    {filteredMovie.map((movie) => {*/}
-                    {/*     return <NewCategory movie={movie.title}/>*/}
-                    {/*    })}*/}
-                    {/*</ul>*/}
-                </div>
-            </div>
-        )
-    }
+				<div>
+					{/*<ul >*/}
+					{/*    {filteredMovie.map((movie) => {*/}
+					{/*     return <NewCategory movie={movie.title}/>*/}
+					{/*    })}*/}
+					{/*</ul>*/}
+				</div>
+			</div>
+		);
+	}
 }
