2cbeaf6747e1a77fafb341092246799254c282ea
Nathaniel Stack <nathanielstack@Nathaniels-MacBook-Pro.local>
2019-10-23 12:11:10 -0700

    posters populating as links on show category page

--- trailertracker/src/OneCategory/OneCategory.js
+++ trailertracker/src/OneCategory/OneCategory.js
@@ -1,46 +1,45 @@
 import React, { Component } from "react";
 import { Link, Route } from "react-router-dom";
 // import catList from "../CategoryList/categoryData.json";
-import UpdateCategory from "../UpdateCategory/UpdateCategory"
-import axios from 'axios'
+import UpdateCategory from "../UpdateCategory/UpdateCategory";
+import axios from "axios";
 
 export default class OneCategory extends Component {
-  constructor() {
-    super()
-    this.state = {
-      movies: []
-    }
-  }
-  componentDidMount() {
-    // axios.get(`http://localhost:8080/Category/${"Action"}`)
-    axios.get(`http://localhost:8080/Category/${this.props.match.params.title}`)
-      .then(res => {
-        console.log(res.data.movies);
+	constructor() {
+		super();
+		this.state = {
+			movies: []
+		};
+	}
+	componentDidMount() {
+		// axios.get(`http://localhost:8080/Category/${"Action"}`)
+		axios
+			.get(`http://localhost:8080/Category/${this.props.match.params.title}`)
+			.then(res => {
+				console.log(res.data.movies);
 
-        this.setState({ movies: res.data.movies });
+				this.setState({ movies: res.data.movies });
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 
-      })
-      .catch(error => {
-        console.log(error);
-      })
+	render() {
+		let list = this.state.movies.map(movie => {
+			return (
+				<div>
+					<a href={`/movie/${movie.title}`}>
+						<img src={movie.poster}></img>
+					</a>
+				</div>
+			);
+		});
 
-  }
-
-  render() {
-    let list = this.state.movies.map(movie => {
-      return (
-        <div>
-          <img src={movie.poster}></img>
-        </div>
-      )
-    })
-
-    return (
-      <div>
-        {list}
-      <h2>hello</h2>
-     </div>
-
-    )
-  }
+		return (
+			<div>
+				{list}
+			</div>
+		);
+	}
 }
