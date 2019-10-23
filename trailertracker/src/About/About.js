ee7bb66c4287378169328cb471c897df33a72043
Shamira Jones <shamirajones@shamiras-mbp.home>
2019-10-23 00:52:36 -0400

    Added my about info

--- trailertracker/src/About/About.js
+++ trailertracker/src/About/About.js
@@ -10,7 +10,7 @@ export default class About extends Component {
                 <p></p>
                 <a href="https://www.linkedin.com/in/shamira-jones/">
                     <h3>Shamira Jones</h3></a>
-                <p></p>
+                <p>Shamira is a Graphic/ Web Designer, Full Stack Web Developer and Guitar player. She loves to turn complex concepts into clean, functional, beautiful realities.Her passion to create is motivated by her belief that part of truly living is to create. </p>
                 <a href="https://www.spiano.dev/">
                     <h3>Sarah Panaligan</h3></a>
                 <p>Sarah is a full stack software engineer with a relentless work ethic and an exceptional ability to adapt to new environments. She is a self-disciplined, resourceful, and efficient programmer who has an eye for design so the final look of the product is as good as the code within the page. </p>
