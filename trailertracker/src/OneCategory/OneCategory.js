90d55a86514a858ac52af1790435d9aa221eab90
James Opara <jamesopara@Jamess-MacBook-Pro.local>
2019-10-23 14:04:24 -0500

    cleaning up code

--- trailertracker/src/OneCategory/OneCategory.js
+++ trailertracker/src/OneCategory/OneCategory.js
@@ -27,93 +27,17 @@ export default class OneCategory extends Component {
   }
 
   render() {
-
-    // console.log(this.state.movies.movies)
-    // let movietitle = this.state.movies;
-    // console.log(this.state.movies)
-
-    let movieArray = this.state.movies.movies || '';
-
-
-    // console.log(movieArray)
-    if (movieArray) {
-      // console.log(movieArray.length)
-      let moviePosters = [];
-      for (let i = 0; i < movieArray.length; i++) {
-        moviePosters.push(movieArray[i].poster);
-
-      }
-
-      console.log(moviePosters);
-
-
-      let listOfPosters = moviePosters.map(poster => {
-
-        console.log(poster);
-        return (
-          <a href={`/movie`}>
-            {/* Change this */}
-            <img className="poster" key={poster} src={poster} alt="poster" />
-          </a>
-        );
-      });
-
-    }
-
+    let list = this.state.movies.map(movie => {
+      return (
+        <div>
+          <img src={movie.poster}></img>
+        </div>
+      )
+    })
 
     return (
-      <div>{this.listOfPosters}</div>
+      <div>{list}</div>
 
     )
-    // let chosenCategory = catList.find(
-    //   element => element.title === this.props.match.params.title
-    // );
-
-
-    // let moviePosters = [];
-    // for (let i = 0; i < chosenCategory.movies.length; i++) {
-    //   moviePosters.push(chosenCategory.movies[i].poster);
-    // }
-    // console.log(moviePosters);
-    // console.log(chosenCategory);
-
-    // let poster = moviePosters.map(poster => {
-    //   console.log(poster);
-    //   return (
-    //     <a href={`/movie`}>
-    //       {/* Change this */}
-    //       <img className="poster" key={poster} src={poster} alt="poster" />
-    //     </a>
-    //   );
-    // });
-
-    // let movieYears = [];
-    // for (let i = 0; i < chosenCategory.movies.length; i++) {
-    //   movieYears.push(chosenCategory.movies[i].releaseYear);
-    // }
-
-    // console.log(chosenCategory)
-
-    // return (
-    //   <div>
-    //     <form id="viewForm">
-    //       <h3 className="homeheader">The Chosen Category is {chosenCategory.title}</h3>
-    //       <div>
-    //         {poster}
-    //         <br></br>
-    //       </div>
-    //       <Link to="/"><button className="backtoHome">Back To Home</button></Link>
-
-
-
-
-    //       <Link to={`/Category/update/${chosenCategory.title}`}><button className="updatecat">
-    //         Update this Category  </button></Link>
-    //     </form>
-    //     <main>
-    //       <Route path="/category/update/:title" exact={true} render={routerProps => <UpdateCategory match={routerProps.match} />} />
-    //     </main>
-    //   </div>
-    // );
   }
 }
