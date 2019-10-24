56d6934c6acfb21c7cf2f2f3a4fd4b420231c747
Nathaniel Stack <nathanielstack@Nathaniels-MacBook-Pro.local>
2019-10-24 10:03:20 -0700

    Updates

---
+++ precache-manifest.9f9617b2af37cb53087872d747967c4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204ec58fd66405a236f3c5501f386fe5",
+    "url": "/trailer-tracker-frontend/index.html"
+  },
+  {
+    "revision": "aa2bef6c30de19761aa5",
+    "url": "/trailer-tracker-frontend/static/css/main.448d14d8.chunk.css"
+  },
+  {
+    "revision": "c393ec467db5d3bf98d1",
+    "url": "/trailer-tracker-frontend/static/js/2.b9a1b819.chunk.js"
+  },
+  {
+    "revision": "aa2bef6c30de19761aa5",
+    "url": "/trailer-tracker-frontend/static/js/main.f009b0f0.chunk.js"
+  },
+  {
+    "revision": "ad1167a803dab03c8217",
+    "url": "/trailer-tracker-frontend/static/js/runtime-main.c61ca98f.js"
+  }
+]);
