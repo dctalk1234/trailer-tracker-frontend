29db91bd943189ecae7245a3de29e3cfd1e427c7
Nathaniel Stack <nathanielstack@Nathaniels-MacBook-Pro.local>
2019-10-25 08:29:16 -0700

    Updates

--- precache-manifest.a3cbc8ba7ba435a5b116e941e4ae2e29.js
+++ precache-manifest.a3cbc8ba7ba435a5b116e941e4ae2e29.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ea8f130ca2ee9bcc9ec5ec1c7f2d0473",
+    "revision": "974430fb4eafae3586fe17e008fc20df",
     "url": "/trailer-tracker-frontend/index.html"
   },
   {
-    "revision": "c58bd945f50edcd2457d",
+    "revision": "74c9b339b5c999f83be8",
     "url": "/trailer-tracker-frontend/static/css/main.74400887.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/trailer-tracker-frontend/static/js/2.ce69cc4e.chunk.js"
   },
   {
-    "revision": "c58bd945f50edcd2457d",
-    "url": "/trailer-tracker-frontend/static/js/main.d42b93f5.chunk.js"
+    "revision": "74c9b339b5c999f83be8",
+    "url": "/trailer-tracker-frontend/static/js/main.eaeea9d8.chunk.js"
   },
   {
     "revision": "ad1167a803dab03c8217",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trailer-tracker-frontend/precache-manifest.e8b75d51ae4b44c791cfbeaae5b26ef4.js"
+  "/trailer-tracker-frontend/precache-manifest.a3cbc8ba7ba435a5b116e941e4ae2e29.js"
 );
 
 self.addEventListener('message', (event) => {
